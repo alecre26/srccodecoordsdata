a705172b633d3843321fb94f8f93e124fb1578cd
alecre26 <53287617+alecre26@users.noreply.github.com>
2019-07-30 01:46:25 -0400

    App created
    App generates x an y coordinates based on mouse clicks inside the pygame window and stores the generated x y coords onto a .txt file

---
+++ get_coords.py
@@ -0,0 +1,41 @@
+import pygame
+
+
+class Coordinates:
+    def __init__(self):
+        self.width = 500
+        self.height = 500
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.get_xy = []
+        self.pos_xy = []
+
+    def run(self):
+        run = True
+        while run:
+            timer = pygame.time.delay(100)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    run = False
+
+            self.pos_xy = pygame.mouse.get_pos()
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                self.get_xy.append(self.pos_xy)
+                print(self.pos_xy)
+
+            with open('coords.txt', 'w') as wf:
+                for itm_pos in self.get_xy:
+                    for itm in itm_pos:
+                        wf.write(
+                            'X_Coord: {} Y_Coord: {}\n'.format(itm_pos[0], itm_pos[1]))
+                        pygame.draw.circle(
+                            self.screen, (255, 255, 0), (itm_pos[0], itm_pos[1]), 8, 0)
+                        pygame.display.update()
+
+        self.screen.fill((0, 0, 0))
+        pygame.display.update()
+
+
+pygame.quit()
+
+get_coord = Coordinates()
+get_coord.run()
